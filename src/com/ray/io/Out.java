11977758219631be8f9d4d4e234ce3f3aaa06606
Ray <Rays1990@163.com>
2018-11-26 12:28:35 +0800

    Add

--- src/com/ray/io/Out.java
+++ src/com/ray/io/Out.java
@@ -3,113 +3,89 @@ package com.ray.io;
 import java.util.Arrays;
 
 /**
- * 锟皆斤拷锟侥简化憋拷准锟斤拷锟斤拷母锟斤拷锟斤拷锟�
+ * 自定义输出流辅助类
  * @author Ray
  *
  */
 public class Out {
     
+    
+    /** 输出并换行 */ public static <T> void p() { System.out.println(); }
+    /** 输出并换行 */ public static <T> void p(T t) { System.out.println(t); }
+    /** 输出不换行 */ public static <T> void pt(T t) { System.out.print(t); }
+    /** 格式化输出 */ public static <T> void pf(String s, Object...args){ System.out.printf(s, args); }
+    /** 输出间隔符 */ public static void sep() { sep(100); }
+    
     /**
-     * 锟斤拷印锟斤拷锟斤拷锟斤拷
-     * @param t
+     * 输出间隔符
+     * @param size
      */
-    public static <T> void p() {
-        System.out.println();
+    public static void sep(int size) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) sb.append("=");
+        p(sb.toString());
     }
-	
-	/**
-	 * 锟斤拷印锟斤拷锟斤拷锟斤拷
-	 * @param t
-	 */
-	public static <T> void p(T t) {
-		System.out.println(t);
-	}
-	
-	
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static <T> void p(T[] t)      { p(Arrays.toString(t)); }
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static void p(boolean[] t)    { p(Arrays.toString(t)); }
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static void p(short[] t)      { p(Arrays.toString(t)); }
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static void p(int[] t)        { p(Arrays.toString(t)); }
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static void p(long[] t)       { p(Arrays.toString(t)); }
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static void p(double[] t)     { p(Arrays.toString(t)); }
-	/** 锟斤拷印锟斤拷锟介并锟斤拷锟斤拷 */public static void p(char[] t)       { p(Arrays.toString(t)); }
-	
-	/**
-	 * 锟斤拷印锟斤拷维锟斤拷锟斤拷
-	 * @param arr
-	 * @param fmt
-	 */
-	public static void p(int[][] arr, String fmt) {
-	    for (int[] is : arr) {
-            for (int i : is) {
-                Out.pf(fmt, i);
-            }
-            Out.p();
-        }
-	}
-	
+
+    /************************************************************************************* 数组相关方法 ****************************************************/
+    
+    /** 打印数组 */public static <T> void p(T[]       t) { p(Arrays.toString(t)); }
+    /** 打印数组 */public static void     p(boolean[] t) { p(Arrays.toString(t)); }
+    /** 打印数组 */public static void     p(short[]   t) { p(Arrays.toString(t)); }
+    /** 打印数组 */public static void     p(int[]     t) { p(Arrays.toString(t)); }
+    /** 打印数组 */public static void     p(long[]    t) { p(Arrays.toString(t)); }
+    /** 打印数组 */public static void     p(double[]  t) { p(Arrays.toString(t)); }
+    /** 打印数组 */public static void     p(char[]    t) { p(Arrays.toString(t)); }
+    
    /**
-     * 锟斤拷印锟斤拷维锟斤拷锟斤拷
+     * 打印二维数组
      * @param arr
      * @param fmt
      */
     public static <T> void p(T[][] arr, String fmt) {
         for (T[] is : arr) {
-            for (T i : is) {
+            for (T i : is)
                 Out.pf(fmt, i);
-            }
             Out.p();
         }
     }
-	
-	   /**
-     * 锟斤拷印锟斤拷维锟斤拷锟斤拷
+
+    /**
+     * 打印二维数组
+     * @param arr
+     * @param fmt
+     */
+    public static void p(int[][] arr, String fmt) {
+        for (int[] is : arr) {
+            for (int i : is)
+                Out.pf(fmt, i);
+            Out.p();
+        }
+    }
+
+    /**
+     * 打印二维数组
+     * @param arr
+     * @param fmt
+     */
+    public static void p(boolean[][] arr, String fmt) {
+        for (boolean[] is : arr) {
+            for (boolean i : is)
+                Out.pf(fmt, i);
+            Out.p();
+        }
+    }
+    
+    /**
+     * 打印二维数组
      * @param arr
      * @param fmt
      */
     public static void p(double[][] arr, String fmt) {
         for (double[] is : arr) {
-            for (double i : is) {
+            for (double i : is)
                 Out.pf(fmt, i);
-            }
             Out.p();
         }
     }
-	
-	
-	/**
-	 * 锟斤拷式锟斤拷锟斤拷锟�
-	 * @param s
-	 * @param args
-	 */
-	public static <T> void pf(String s, Object...args){
-		System.out.printf(s, args);
-	}
-	
-	/**
-	 * 只锟斤拷印锟斤拷锟斤拷锟斤拷
-	 * @param t
-	 */
-	public static <T> void pt(T t) {
-		System.out.print(t);
-	}
-	
-	/**
-	 * 锟斤拷印锟街革拷锟斤拷
-	 * 
-	 */
-	public static void sep() {
-		sep(100);
-	}
-	
-	/**
-	 * 锟斤拷印锟街革拷锟斤拷
-	 * @param size
-	 */
-	public static void sep(int size) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < size; i++) sb.append("=");
-		p(sb.toString());
-	}
-	
+
 }
