54378a062dcd7e116a79f5f3ec08e818717055a7
Ray <Rays1990@163.com>
2019-07-11 11:13:49 +0800

    add assert

---
+++ src/com/ray/util/Assert.java
@@ -0,0 +1,26 @@
+package com.ray.util;
+
+/**
+ * Simple Assert Util
+ * 
+ * 
+ * @author rays1
+ *
+ */
+public class Assert {
+    
+    public static <T> void assertNotNull(T obj) {
+        if (obj == null) exception("object is null");
+    }
+    
+    public static <T extends Comparable<T>> void assertEquals(T o1, T o2) {
+        if (o1 == null) exception("o1 is null");
+        if (o2 == null) exception("o2 is null");        
+        if (!o1.equals(o2)) exception("o1 is not equals o2");
+    }
+    
+    private static void exception(String msg) {
+        throw new RuntimeException(msg);
+    }
+    
+}
