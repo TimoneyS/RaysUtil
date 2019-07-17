1f01af1ac5aa8e03b90e2ff263f45be632466db0
Ray <Rays1990@163.com>
2019-07-17 14:32:41 +0800

    add

--- src/com/ray/util/Assert.java
+++ src/com/ray/util/Assert.java
@@ -13,7 +13,7 @@ public class Assert {
         if (obj == null) exception("object is null");
     }
     
-    public static <T extends Comparable<T>> void assertEquals(T o1, T o2) {
+    public static <T> void assertEquals(T o1, T o2) {
         if (o1 == null) exception("o1 is null");
         if (o2 == null) exception("o2 is null");        
         if (!o1.equals(o2)) exception("o1 is not equals o2");
